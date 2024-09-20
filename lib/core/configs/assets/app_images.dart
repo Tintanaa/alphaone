09fafc559669a8d3bb6f618d2b31ec0bf5d21149
Tintanaa <miiagkov123456@gmail.com>
2024-09-20 22:39:12 +0600

    feat: theme changer (dark/ light)

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+  static const String introBG = '${basePath}intro_bg.jpg';
+  static const String chooseModeBG = '${basePath}choosemode.jpg';
+}
--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,15 +1,41 @@
 import 'package:alphaone/core/configs/assets/app_vectors.dart';
+import 'package:alphaone/presentation/intro/pages/start_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset(AppVectors.logo),
+        child: SvgPicture.asset(
+          AppVectors.logo,
+        ),
+      ),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future<void>.delayed(const Duration(seconds: 2));
+    if (!mounted) return;
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) => const GetStartedPage(),
       ),
     );
   }
